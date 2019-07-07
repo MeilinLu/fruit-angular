d03f77ac27a89f5cbe7287544f9bbdc980122a15
MeilinLu <meilinlu86@gmail.com>
2019-07-07 12:51:52 -0400

    Data Binding - #10 - {{homeTitle}}

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  homeTitle = "Welcome to the fruit-app homepage!";
   constructor() { }
 
   ngOnInit() {
