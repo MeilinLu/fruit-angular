5311fa6f8a0b3d3682cf9f7449b5dd5b1e719a78
MeilinLu <meilinlu86@gmail.com>
2019-07-09 19:31:35 -0400

    Directives Introduction - #19A - <p [ngClass]=''...''>

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -7,12 +7,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./directory.component.scss']
 })
 export class DirectoryComponent implements OnInit {
-
+  // #18 Route Params
+  /*
   fruit: string;
-
   constructor(private route: ActivatedRoute) {
     this.fruit = route.snapshot.params['fruit'];
   }
+  */
+  // #19 Directives Introduction
+  
+  constructor(){ }
 
   ngOnInit() {
   }
