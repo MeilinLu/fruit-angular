d5268e4e302a2249c87aeabe412a055126824e16
MeilinLu <meilinlu86@gmail.com>
2019-07-09 22:53:37 -0400

    Structural Directive *ngFor- #20 - Attribute Directive [ngStyle]

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -16,8 +16,19 @@ export class DirectoryComponent implements OnInit {
   */
 
   // #19 Directives Introduction
-  classes = {'blue':true, 'red': false, 'underline': true};
-  test = true;
+  // classes = {'blue':true, 'red': false, 'underline': true};
+  // test = true;
+
+  // #20 ngFor Directive
+  fruits = [
+    {name: "Apple", color: "red", price:"$1.99"},
+    {name: "Apple", color: "green", price:"$1.99"},
+    {name: "Cherry", color: "yellow", price:"$9.99"},
+    {name: "Banana", color: "yellow", price:"$0.99"},
+    {name: "Blueberry", color: "purple", price:"$5.99"},
+    {name: "Pear", color: "green", price:"$2.99"}
+  ]
+
   constructor(){ }
 
   ngOnInit() {
